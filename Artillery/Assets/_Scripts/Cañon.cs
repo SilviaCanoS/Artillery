3f0273c04f79bd4e5779722d16e063c94a0c07bb
SilviaCanoS <99064925+SilviaCanoS@users.noreply.github.com>
2023-07-10 01:14:39 -0600

    InputSystem
    inputActions

--- Artillery/Assets/_Scripts/Cañon.cs
+++ Artillery/Assets/_Scripts/Cañon.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Cañon : MonoBehaviour
 {
@@ -16,6 +17,28 @@ public class Cañon : MonoBehaviour
     private float rotacion;
     private int cont = 1;
 
+    public CañonControl cañonControl;
+    private InputAction apuntar, modificarFuerzaDisparo, disparar;
+
+    private void Awake()
+    {
+        cañonControl = new CañonControl();
+    }
+
+    private void OnEnable()
+    {
+        apuntar = cañonControl.Cañon.Apuntar;
+        modificarFuerzaDisparo = cañonControl.Cañon.ModificarFuerzaDisparo;
+        disparar = cañonControl.Cañon.Disparar;
+        apuntar.Enable();
+        modificarFuerzaDisparo.Enable();
+        disparar.Enable();
+
+        //disparar.started //cuando se inicia el evento
+        disparar.performed += Disparar; //mientras ocurre
+        //disparar.canceled //cuano termina
+    }
+
     private void Start()
     {
         puntaCañon = transform.Find("PuntaCañon").gameObject;
@@ -26,16 +49,22 @@ public class Cañon : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        rotacion += Input.GetAxis("Horizontal") * AdministradorJuego.singletonAdminJuego.VelocidadRotacion;
+        //rotacion += Input.GetAxis("Horizontal") * AdministradorJuego.singletonAdminJuego.VelocidadRotacion;
+        rotacion += apuntar.ReadValue<float>() * AdministradorJuego.singletonAdminJuego.VelocidadRotacion;
 
         //restringe el movimiento del cañon de 0 a 90°
         if (rotacion <= 90 && rotacion >= 0) transform.eulerAngles = new Vector3(rotacion, 90, 0);
         if (rotacion > 90) rotacion = 90;
         if (rotacion < 0) rotacion = 0;
 
-        if (Input.GetKeyDown(KeyCode.Space) 
-            && cont <= AdministradorJuego.singletonAdminJuego.DisparosPorJuego
-            && !bloqueado)
+        //if (Input.GetKeyDown(KeyCode.Space) 
+        //    && cont <= AdministradorJuego.singletonAdminJuego.DisparosPorJuego
+        //    && !bloqueado) 
+    }
+
+    private void Disparar(InputAction.CallbackContext context)
+    {
+        if (cont <= AdministradorJuego.singletonAdminJuego.DisparosPorJuego && !bloqueado)
         {
             cont++;
             GameObject temp = Instantiate(balaPrefab, puntaCañon.transform.position, transform.rotation);
